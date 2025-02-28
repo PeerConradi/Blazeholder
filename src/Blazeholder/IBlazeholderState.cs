feb99c135b61487d21cef13d762f4442068adb09
Peer Conradi <peerconradi@gmail.com>
2025-02-28 21:59:41 +0100

    Projektdateien hinzufügen.

---
+++ src/Blazeholder/IBlazeholderState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazeholder
+{
+	internal interface IBlazeholderState
+	{
+		Task TaskElement { get; }
+
+		Task SetTask(System.Threading.Tasks.Task task);
+	}
+}
